c8f7e173c3c9ff5f4d990d7406932a52e6887e61
theFredward <fventura59@gmail.com>
2023-07-01 01:00:38 -0500

    updated code.py

--- CIRCUITPY/code.py
+++ CIRCUITPY/code.py
@@ -16,7 +16,8 @@ keyboard.col_pins = (board.GP2,board.GP3,board.GP4,board.GP5,board.GP6,board.GP7
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 # Layer keys
-Temp = KC.MO(1)
+LYR_1 = KC.MO(1)
+LYR_2 = KC.MO(2)
 # Custom key combos
 C_EDGE = send_string("msedge")
 RUN_EDGE = simple_key_sequence( ( KC.LWIN(KC.R), KC.MACRO_SLEEP_MS(1000), C_EDGE, KC.MACRO_SLEEP_MS(1000), KC.ENT) )
@@ -28,12 +29,16 @@ keyboard.keymap = [
         [ 
           KC.Q,KC.W,KC.E,KC.R,KC.T,KC.Y,KC.U,KC.I,KC.O,KC.P,
           KC.A,KC.S,KC.D,KC.F,KC.G,KC.H,KC.J,KC.K,KC.L,KC.QUOT,
-          KC.Z,KC.X,KC.C,KC.V,KC.B,KC.N,KC.M,KC.COMM,KC.DOT,KC.ENT,
+          KC.LSFT,KC.Z,KC.X,KC.C,KC.V,KC.B,KC.N,KC.M,KC.COMM,KC.DOT,
+          KC.TAB,KC.LCTL,KC.LALT,KC.LGUI,LYR_1,KC.SPC,LYR_2,KC.BSPC,KC.DEL,KC.ENT
 
         ],
         #Layer 1
         [
-           TST, 
+            KC.N1,KC.N2,KC.N3,KC.HOME,KC.END,KC.PGUP,KC.PGDN,KC.PSLS,KC.PMNS,KC.PAST,
+            KC.N4,KC.N5,KC.N6,KC.LEFT,KC.DOWN,KC.UP,KC.RIGHT,KC.SLSH,KC.GRV,KC.CAPS,
+            KC.N7,KC.N8,KC.N9,
+            KC.N0,
         ]
         
 ]
